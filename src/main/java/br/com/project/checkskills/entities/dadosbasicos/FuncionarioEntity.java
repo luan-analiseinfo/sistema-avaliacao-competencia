e23d285faa1193207c9a751cbad2b3a6e5ecf476
luan-analiseinfo <luan@analiseinfo.com.br>
2016-10-30 15:20:14 -0200

    campos de data adicionados

--- src/main/java/br/com/project/checkskills/entities/dadosbasicos/FuncionarioEntity.java
+++ src/main/java/br/com/project/checkskills/entities/dadosbasicos/FuncionarioEntity.java
@@ -1,5 +1,7 @@
 package br.com.project.checkskills.entities.dadosbasicos;
 
+import java.util.Date;
+
 import javax.persistence.AttributeOverride;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -9,7 +11,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 
 import org.springframework.stereotype.Component;
 
@@ -18,43 +23,56 @@ import br.com.project.checkskills.entities.avaliacao.AvaliacaoEntity;
 import br.com.project.checkskills.utils.BaseEntity;
 
 @Entity
-@Table(name="TB_FUNCIONARIO")
+@Table(name = "TB_FUNCIONARIO")
 @AttributeOverride(name = "id", column = @Column(name = "ID_FUNCIONARIO"))
-@Component(value="funcionarioEntity")
+@Component(value = "funcionarioEntity")
 public class FuncionarioEntity extends BaseEntity<Long> {
 
 	private static final long serialVersionUID = 1L;
-	
-	@Column(name="NOME",nullable=false,length=75)
-	@NotNull(message="Campo obrigatório")
+
+	@Column(name = "NOME", nullable = false, length = 75)
+	@NotNull(message = "Campo obrigatório")
 	private String nome;
-	
-	@Column(name="SEXO",length=1,nullable=false)
-	@NotNull(message="Campo obrigatório")
+
+	@Column(name = "SEXO", length = 1, nullable = false)
+	@NotNull(message = "Campo obrigatório")
 	private String sexo;
+
+	@Past
+	@Column(name = "DATA_NASCIMENTO", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataNasc;
 	
-	@OneToOne( cascade={CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REMOVE}, 
-			optional = false, fetch = FetchType.EAGER)
-	@JoinColumn(name="ID_FUNCIONARIO")
+	@Past
+	@Column(name = "DATA_ADMISSAO", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataAdmissao;
+
+	@Past
+	@Column(name = "DATA_DEMISSAO")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataDemissao;
+	
+	@OneToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE,
+			CascadeType.REMOVE }, optional = false, fetch = FetchType.EAGER)
+	@JoinColumn(name = "ID_FUNCIONARIO")
 	private UsuarioEntity usuarioEntity;
 
-	@ManyToOne(cascade={CascadeType.MERGE}, 
-			optional = false, fetch = FetchType.EAGER)
-	@JoinColumn(name="ID_CARGO")
+	@ManyToOne(cascade = { CascadeType.MERGE }, optional = false, fetch = FetchType.EAGER)
+	@JoinColumn(name = "ID_CARGO")
 	private CargoEntity cargo;
 
-	@OneToOne(mappedBy="funcionario")
+	@OneToOne(mappedBy = "funcionario")
 	private AvaliacaoEntity avaliacao;
 
-	
 	public String getSexo() {
 		return sexo;
 	}
-	
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-	
+
 	public CargoEntity getCargo() {
 		return cargo;
 	}
@@ -79,6 +97,18 @@ public class FuncionarioEntity extends BaseEntity<Long> {
 		this.nome = nome;
 	}
 
+
+	
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+
+	
 	public UsuarioEntity getUsuarioEntity() {
 		return usuarioEntity;
 	}
@@ -87,11 +117,28 @@ public class FuncionarioEntity extends BaseEntity<Long> {
 		this.usuarioEntity = usuarioEntity;
 	}
 	
-	@Override
-	public String toString() {
-	    return String.format("%s[id=%d]", getClass().getSimpleName(), getId());
+	
+
+	public Date getDataAdmissao() {
+		return dataAdmissao;
+	}
+
+	public void setDataAdmissao(Date dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+
+	public Date getDataDemissao() {
+		return dataDemissao;
+	}
+
+	public void setDataDemissao(Date dataDemissao) {
+		this.dataDemissao = dataDemissao;
 	}
 	
-	
+
+	@Override
+	public String toString() {
+		return String.format("%s[id=%d]", getClass().getSimpleName(), getId());
+	}
 
 }
